67835f091d6ad54683c8be3a1e9fe85be1354a24
Sibonsio <siboniso1sekute@gmail.com>
2025-11-15 22:06:53 +0200

    complete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,9 @@
 import './Cards.css'
 import Card from './Card.jsx'
-import { useState, useEffect } from 'react'
-import instance from '../config/axios.jsx'
-
-
 
 const Cards = ({ darkMode, allData, toggleClick, handleClick }) => {
-
-
     const showAllCards = allData.map((card) => {
-        return <Card key={card._id} lightBg={darkMode && 'lightBg'} darkText={darkMode && 'darkText'} textColor={darkMode && 'textColor'} name={card.name} description={card.description} logo={card.logo} isActive={card.isActive} id={card._id} toggleClick={toggleClick} handleClick={handleClick} />
+        return <Card key={card._id} lightBg={darkMode && 'lightBg'} darkText={darkMode && 'darkText'} textColor={darkMode && 'textColor'} name={card.name} description={card.description} logo={card.image} isActive={card.isActive} id={card._id} toggleClick={toggleClick} handleClick={handleClick} />
     })
 
     return (<div className='grid-container'>{showAllCards}</div>)
