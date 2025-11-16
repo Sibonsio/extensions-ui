144404f56122dce51b52921047e9bb3b1315f544
Sibonsio <siboniso1sekute@gmail.com>
2025-11-16 08:45:04 +0200

    project complete

---
+++ src/components/newCard.jsx
@@ -0,0 +1,9 @@
+import '../components/Card.css'
+import '../components/formCard.css'
+const newCard = ({ handleClick, id, lightBg, darkText, textColor, toggleClick, isActive }) => {
+    return (
+        <></>
+    )
+}
+
+export default newCard
