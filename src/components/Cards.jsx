22a9c511b45930439d971b1ebbe299dc8e549136
Sibonsio <siboniso1sekute@gmail.com>
2025-11-16 11:20:09 +0200

    updated card.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,9 +2,9 @@ import './Cards.css'
 import Card from './Card.jsx'
 
 const Cards = ({ darkMode, allData, toggleClick, handleClick }) => {
-    const publicPath = 'http://localhost:5002/public/'
+    const publicPath = 'http://localhost:5002/'
     const showAllCards = allData.map((card) => {
-        return <Card key={card._id} lightBg={darkMode && 'lightBg'} darkText={darkMode && 'darkText'} textColor={darkMode && 'textColor'} name={card.name} description={card.description} logo={`${publicPath}${card.image.filename}`} isActive={card.isActive} id={card._id} toggleClick={toggleClick} handleClick={handleClick} />
+        return <Card key={card._id} lightBg={darkMode && 'lightBg'} darkText={darkMode && 'darkText'} textColor={darkMode && 'textColor'} name={card.name} description={card.description} logo={`${publicPath}${card.image.path}`} isActive={card.isActive} id={card._id} toggleClick={toggleClick} handleClick={handleClick} />
     })
 
     return (<div className='grid-container'>{showAllCards}</div>)
